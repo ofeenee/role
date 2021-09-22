7ac4c779135633d23cccd8fe6315610ca1a13ec0
ofeenee <ofeenee@gmail.com>
2021-09-22 20:17:29 +0300

    basic role functionality

---
+++ Role.js
@@ -0,0 +1,81 @@
+
+function validatePhone(string) {
+  try {
+    if (typeof string !== 'string' || !string) throw new Error('value is invalid.');
+    return isMobilePhone(string, 'any', { strictMode: true });
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+function Role({
+  role = 'basic',
+  ROLES = ['admin', 'moderator', 'vip', 'premium', 'member', 'basic']
+} = { role: 'basic', ROLES: ['admin', 'moderator', 'vip', 'premium', 'member', 'basic'] }) {
+  try {
+    if (new.target === undefined) return new Role({role, ROLES});
+
+    Object.freeze(ROLES);
+
+    // HELPER FUNCTIONS
+    function validateRole(string) {
+      try {
+        if (typeof string === 'string' && string)
+          return ROLES.includes(string.trim().toLowerCase());
+        else return false;
+      }
+      catch (error) {
+        throw error;
+      }
+    }
+
+    // return new instance of Role <role object>
+    return Object.defineProperties(this, {
+      role: {
+        value: role,
+        configurable: true
+      },
+      set: {
+        value: (string) => {
+          try {
+            string = string?.trim()?.toLowerCase();
+            if (typeof string === 'string' && ROLES.includes(string.toLowerCase())) {
+              return Object.defineProperty(this, 'role', {
+                value: string,
+                configurable: true
+              });
+            }
+            else {
+              throw new Error('role value is invalid.');
+            }
+          } catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      get: {
+        value: () => {
+          if (this.role) return this.role;
+          else return null;
+        },
+        enumerable: true
+      },
+      validate: {
+        value: validateRole,
+        enumerable: true
+      },
+    });
+  }
+  catch (error) {
+    throw error;
+  }
+}
+
+export default Role;
+
+// const role = new Role();
+// console.log(role);
+// console.log(role.get());
+// console.log(role.validate('moderator'));
